277434a538cc8b9629a115d826342c4f39721463
Alexey <ipodbot@rambler.ru>
2013-10-28 19:59:58 +0400

    Strange things happening.

--- src/First_period.c
+++ src/First_period.c
@@ -6,7 +6,7 @@
 
 #define VALID_ARGS "I:P:N:L:h"
 
-#define STEPS 1000
+#define STEPS 100
 
 #define HELP "Help\nL <lambda>\nN <N of devices>"
 /*
@@ -38,8 +38,8 @@ void *tester(void *args){
 int main(int argc, char *argv[]){
   double *devTimes = NULL, *pLambdas = NULL, avgT = 0.0, stepper = 0.0, maxTime = 0.0, lambda = 0.0;
   int i = 0;
-  unsigned int nDevs = 0, j = 1, alive = 0, failFunc = 0, nClasses = 0, *pNSummons = NULL, k = 0;
-  FILE *histogram = NULL, *classes = NULL;
+  unsigned int nDevs = 0, j = 1, l = 0, alive = 0, failFunc = 0, nClasses = 0, *pNSummons = NULL, k = 0, rander = 0, nSteps = 0;
+  FILE *histogram = NULL, *classes = NULL, *randInit = NULL;
 
   while((i = getopt(argc, argv, VALID_ARGS)) != -1){
     switch(i){
@@ -105,6 +105,16 @@ int main(int argc, char *argv[]){
     }
   }
 
+  randInit = fopen("/dev/urandom", "rb");
+
+  if(!randInit){
+    (void)puts("No urandom device.");
+  }else{
+    (void)fread(&rander, 1 , sizeof(unsigned int), randInit);
+    (void)srand(rander);
+    (void)fclose(randInit);
+  }
+
   if(nClasses == 1){
     pLambdas = calloc(1, sizeof(double));
     pNSummons = calloc(1, sizeof(unsigned int));
@@ -121,6 +131,7 @@ int main(int argc, char *argv[]){
 
   devTimes = calloc(nDevs, sizeof(double));
 
+  /* First period of life. */
   i = *pNSummons;
 
   for(j = 0, k = 0; j < nDevs; j++){
@@ -144,19 +155,79 @@ int main(int argc, char *argv[]){
 
   histogram = fopen("histogram_furst.dat", "w");
 
+  nSteps = STEPS;
+
   /* Calculate R(t) and Lambda(t) */
-  for(stepper = (double)maxTime/(double)STEPS; stepper < maxTime; stepper += maxTime/STEPS){
+  for(stepper = maxTime/nSteps; stepper <= maxTime; stepper += maxTime/nSteps){
     alive = 0;
     failFunc = 0;
     for(j = 0; j < nDevs; j++){
-      if(*(devTimes + j) > stepper){
+      if(*(devTimes + j) >= stepper){
 	alive++;
-	if(*(devTimes + j) < (stepper + maxTime/STEPS)){
+	if(*(devTimes + j) < (stepper + maxTime/nSteps)){
 	  failFunc++;
 	}
       }
     }
-    (void)fprintf(histogram, "%f\t%f\t%d\t%f\n", stepper, (double)alive/(double)nDevs, failFunc, (((double)failFunc)/(double)alive)*(double)STEPS/(double)maxTime);
+    (void)fprintf(histogram, "%f\t%f\t%f\n", stepper, (double)alive/(double)nDevs, ((double)failFunc/(double)alive)*((double)nSteps/(double)maxTime));
+  }
+
+  (void)fclose(histogram);
+  maxTime = 0;
+  /* Second period of life. */
+  i = *pNSummons;
+
+  for(j = 0, nDevs = 0; j < nClasses; j++){
+    if(nDevs < *(pNSummons + j)){
+      nDevs = *(pNSummons + j);
+    }
+  }
+ 
+  (void)free(devTimes);
+  devTimes = calloc(nDevs, sizeof(double));
+
+  for(j = 0, k = 0; j < nDevs; j++){
+    if(j > i){
+      i += *(pNSummons + k + 1);
+      k++;
+    }
+    lambda = 0;
+    for(l = 0; l < nClasses; l++){
+      if(*(pNSummons + l) > j){
+        lambda += *(pLambdas + l);
+      }
+    }
+    *(devTimes + j) = -(1.0/lambda)*log((double)(RAND_MAX - rand())/(double)RAND_MAX);
+    avgT += *(devTimes + j);
+#ifdef DEBUG
+    (void)printf("%f\n", *(devTimes + j));
+#endif
+    if(*(devTimes + j) > maxTime){
+      maxTime = *(devTimes + j);
+    }
+  }
+  
+  avgT /= nDevs;
+
+  (void)printf("Average operation time for first stage %f\n", avgT);
+
+  histogram = fopen("histogram_secund.dat", "w");
+
+  nSteps = ceil(maxTime)*10.0;
+
+  /* Calculate R(t) and Lambda(t) */
+  for(stepper = maxTime/nSteps; stepper < maxTime; stepper += maxTime/nSteps){
+    alive = 0;
+    failFunc = 0;
+    for(j = 0; j < nDevs; j++){
+      if(*(devTimes + j) >= stepper){
+	alive++;
+	if(*(devTimes + j) < (stepper + maxTime/nSteps)){
+	  failFunc++;
+	}
+      }
+    }
+    (void)fprintf(histogram, "%f\t%f\t%f\n", stepper, (double)alive/(double)nDevs, ((double)failFunc/(double)alive)*((double)nSteps/(double)maxTime));
   }
 
   (void)fclose(histogram);
